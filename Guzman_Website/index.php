2204545f11c0fb48fa1ef4cdf35d0def0c411c19
jguzman115 <78218977+jguzman115@users.noreply.github.com>
2023-03-10 10:41:07 -0600

    Aded a work in progress new menu, and a button that toggles between a yellow and purple background

---
+++ Guzman_Website/index.php
@@ -0,0 +1,17 @@
+  <?php include("common/document_head.html"); ?>
+  <body onload="startRotation()">
+    <header>
+      <?php
+      include("common/menus.html");
+      ?>
+    </header>
+    <main>
+      <article id="textLeft">
+        <h3>Welcome to Juan's Websie</h3>
+        <p>I am planning for this to be my very first main page and it will replace
+          the one that I currently have
+        </p>
+      </article>
+    </main>
+  </body>
+</html>
