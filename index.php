3cb989bfe3641a812f27ba3bdb88192f0fce257f
jguzman115 <78218977+jguzman115@users.noreply.github.com>
2023-03-17 16:54:32 -0500

    Cleaned unused files, and used php in my index.

--- index.php
+++ index.php
@@ -1,17 +1,56 @@
-  <?php include("common/document_head.html"); ?>
+<!--#include virtual="common/document_head.html"-->
+<!-- index.html for ch03/nature3 -->
+
+<?php include("common/document_head.html");?>
   <body onload="startRotation()">
     <header>
-      <?php
-      include("common/menus.html");
-      ?>
+      <?php include("common/menus.html");?>
     </header>
     <main>
-      <article id="textLeft">
-        <h3>Welcome to Juan's Websie</h3>
-        <p>I am planning for this to be my very first main page and it will replace
-          the one that I currently have
-        </p>
-      </article>
+      <table>
+        <tr>
+          <td colspan="1">
+            <head>
+              <meta charset="utf-5">
+              <title>Juan's Website</title>
+              <link rel="stylesheet" type="text/css" href="pages/css/main.css">
+            </head>
+            <h1>Hi, I'm Juan Guzman</h1>
+            <p>Software Engineering</p>
+          </td>
+          <td colspan="2">
+            <img src="images/spiderman.jpg" alt="Spiderman" width="256" height="256">
+          </td>
+        </tr>
+      </table>
     </main>
   </body>
 </html>
+
+
+<!-- <tr>
+    <td><a href="pages/resume.html">Resume</a></td>
+  </tr>
+  <tr>
+    <td><a href="pages/form.html">Reach Out Form</a></td>
+  </tr>
+  <tr>
+    <td><a href="pages/box.html">Flex Box</a></td>
+  </tr>
+  <tr>
+    <td><a href="pages/feedback.html">Feedback Form</a></td>
+  </tr>
+  <tr>
+    <td><a href="index.php">New Menu (Work in Progress)</a></td>
+  </tr>
+  <button type="button" onclick="myFunction()">Change Color</button>
+
+  <script>
+    function myFunction() {
+      if (document.body.style.backgroundColor != "purple"){
+        document.body.style.backgroundColor = "purple";
+      }else{
+        document.body.style.backgroundColor = "yellow";
+      }
+    }
+  </script> -->
---
+++ index2.php
@@ -0,0 +1,15 @@
+<?php include("common/document_head.html");?>
+  <body onload="startRotation()">
+    <header>
+      <?php
+      include("common/menus.html");
+      ?>
+    </header>
+    <main>
+      <article id="textRight">
+        <h3>Hi, I'm Juan Guzman</h3>
+        <p>Software Engineering</p>
+      </article>
+    </main>
+  </body>
+</html>
